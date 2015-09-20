22284dce3f1774a37810ca1cd54a3e7d386f21bb
Michael Akbaraly <makbaraly@octo.com>
2015-09-21 00:10:33 +0200

    plug authentification to the database (back)

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ var _SECRET = "secret_a_changer";
 var FriendsAPI = require("./api/friendsApi.js");
 var AccountAPI = require("./api/accountApi.js");
 var Datastore = require("nedb");
+var _ = require("lodash");
 
 var app = express();
 
-process.title = "kaching_ba";
-
-var friendsDB = new Datastore({filename: process.env.KACHING_DATABASE_PATH + "/friends.db", autoload: true});
+var friendsDB = new Datastore({filename: process.env.KACHING_DATABASE_PATH +"/friends.db", autoload: true});
 console.log('Database path : ', process.env.KACHING_DATABASE_PATH);
 var friendsAPI = new FriendsAPI(friendsDB);
 
+var usersDB = new Datastore({filename: process.env.KACHING_DATABASE_PATH +"/users.db", autoload: true});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -31,31 +31,23 @@ app.use(corsHelper.setCorsHeaders);
 app.use('/api', expressJwt({secret: _SECRET}));
 
 app.post('/authenticate', function (req, res) {
-    //TODO validate req.body.username and req.body.password
-    //if is invalid, return 401
-    if (req.body === undefined || (!(req.body.login === 'john.doe' && req.body.password === 'foobar'))) {
+  if (req.body !== undefined) {
+    usersDB.findOne({username: req.body.login, password: req.body.password}, function (err, profile) {
+      console.log(profile)
+      if (err || _.isEmpty(profile)) {
         res.send(401, 'Wrong user or password');
-        return;
-    }
-
-    var profile = {
-        first_name: 'John',
-        last_name: 'Doe',
-        email: 'john@doe.com',
-        id: 123
-    };
-
-    // We are sending the profile inside the token
-    var token = jwt.sign(profile, _SECRET, {expiresInMinutes: 60 * 5});
-
-    res.json({token: token});
+      }
+      var token = jwt.sign(profile, _SECRET, {expiresInMinutes: 60 * 5});
+      res.json({username: profile.username, token: token});
+    })
+  }
 });
 
 app.get('/api/restricted', function (req, res) {
-    console.log('user ' + req.user.email + ' is calling /api/restricted');
-    res.json({
-        name: 'foo'
-    });
+  console.log('user ' + req.user.email + ' is calling /api/restricted');
+  res.json({
+    name: 'foo'
+  });
 });
 
 app.get("/api/friends/:username", friendsAPI.getFriends);
@@ -63,5 +55,5 @@ app.get("/api/friends/:username", friendsAPI.getFriends);
 app.get('/api/account', AccountAPI.getAccount);
 
 app.listen(3000, function () {
-    console.info('running on port 3000');
+  console.info('running on port 3000');
 });
