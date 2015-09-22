edb2f243b9595cc3fc2ccf928f21d821976efb37
Michael Akbaraly <makbaraly@octo.com>
2015-09-23 00:15:57 +0200

    retrieve friends by username => dirty mode ON!!! YEAH!!!

--- services/friendsService.js
+++ services/friendsService.js
@@ -5,6 +5,9 @@ module.exports = function (db) {
         if (err) {
           return callback(err);
         }
+        if(docs === undefined || docs.length === 0) {
+          return callback(err);
+        }
         return callback(null, docs[0].friends)
       });
     }
